ae717d682800df3ab2c41e2af7bb629278fcbe3d
RodolfoKaizen <rodolfokaizen@gmail.com>
2025-04-25 14:42:42 -0300

    01

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import './utils/global.js';
+
+import 'dotenv/config.js';
+import express from 'express';
+import cors from 'cors';
+
+import './repository/connection.js';
+
+
+const servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+
+
+
+const PORTA = process.env.PORTA;
+servidor.listen(PORTA, () => console.log(`==> Api subiu na porta ${PORTA}`));
